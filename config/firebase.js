50ea655a0bfa22055ff2fc39ec6bab80dafae489
Angus Panero <aguspanero@gmail.com>
2026-07-01 23:02:41 +0200

    FIX REPLACE ENV

--- config/firebase.js
+++ config/firebase.js
@@ -4,7 +4,7 @@ const userService = {
     "type": process.env.FIREBASE_TYPE,
     "project_id": process.env.FIREBASE_PROJECT_ID,
     "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    "private_key": (process.env.FIREBASE_PRIVATE_KEY || "").split("\\n").join("\n"),
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     "client_id": process.env.FIREBASE_CLIENT_ID,
     "auth_uri": process.env.FIREBASE_AUTH_URI,
